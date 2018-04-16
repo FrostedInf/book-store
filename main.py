9c8b6e18c8f8c3994e097c787959c0bf0d936655
Alexander <alexhernandez840@gmail.com>
2018-04-16 00:59:51 -0500

    add - plantillas: se separo codigo en diferentes archivos para mejor mantenimiento

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import make_response
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
-if __name__ == '__main__'
-app.run(debug = True)
+
+@app.route("/contact")
+def header():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
