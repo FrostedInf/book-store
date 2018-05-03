ddf7c19118184796d44ed689495893abccf796b1
irving <sefiro2016@gmail.com>
2018-05-03 10:49:38 -0500

    Datos de la sql
    Contiene usuarios y Libros

---
+++ datosUsuarioLibros.sql
@@ -0,0 +1,11 @@
+insert into libros (ISBN,titulo,vecesVendido,numeroPaginas,existencia,genero,descripcion,editorial,fechaPublicacion,precio,autor,portada) values ("rak312","La inteligencia emocional",253,200,230,"Psicología","Libro de daniel Goleman","panini",'1987-10-20',350,"Daniel Golleman","a");
+insert into libros (ISBN,titulo,vecesVendido,numeroPaginas,existencia,genero,descripcion,editorial,fechaPublicacion,precio,autor,portada) values ("rak435","El arte de no amargarse la vida",100,300,300,"Psicología","Este libro busca cambiar la manera en que piensan algunas personas","universal",'2015-04-21',500,"Rafel Santandreu","a");
+insert into libros (ISBN,titulo,vecesVendido,numeroPaginas,existencia,genero,descripcion,editorial,fechaPublicacion,precio,autor,portada) values ("rak292","La iliada",600,375,200,"Aventura","Libro de mitologia griega","panini",'1450-01-01',350,"Hombre desconocido","a");
+insert into libros (ISBN,titulo,vecesVendido,numeroPaginas,existencia,genero,descripcion,editorial,fechaPublicacion,precio,autor,portada) values ("rak932","La divina comedia",400,400,350,"Aventura","Libro de dante cervántes","cervántes",'1800-05-17',375,"Dante Cervántes","a");
+insert into libros (ISBN,titulo,vecesVendido,numeroPaginas,existencia,genero,descripcion,editorial,fechaPublicacion,precio,autor,portada) values ("rak143","Benito Mussolini",100,157,100,"Biografia","Biografia del dictador italiano","universal",'1950-05-13',290,"Benito Mussolini","a");
+insert into users (username,imagen,email,password) values ("Paco","pndnte","chatp@crack.com","1234");
+insert into users (username,imagen,email,password) values ("Miranda","pndnte", "miranda32@gmail.com","hola");
+insert into users (username,imagen,email,password) values ("Paula","pndnte", "paulita@politician.com","12");
+insert into users (username,imagen,email,password) values ("Elizabeth","pndnte", "missbeth43@gmail.com","gato");
+insert into users (username,imagen,email,password) values ("Toño","pndnte", "hacker777@hacker.org","kali");
+
